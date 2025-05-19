e150e6e2229fa4586412d17786cacb665af8c973
Asim Riaz <asim.riaz@gmail.com>
2025-05-19 10:56:29 +0500

    Change Route

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 4500
 express()
     .use(express.json())
     .use(cors())
-    .use(express.static(path.join(__dirname, 'dist')))
-    .get('*', (req, res) => res.sendFile(path.join(__dirname, 'dist', 'index.html')))
+    //.use(express.static(path.join(__dirname, 'dist')))
+    .get('/', (req, res) => res.sendFile(path.join(__dirname, 'dist', 'index.html')))
     .use('/api', indexRouter)
     .listen(PORT, () => console.log(`Server is listening on http://localhost:${PORT}`));
