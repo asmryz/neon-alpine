879a27ca8c38f85f79b666428f6cb5e822c1fc0a
Asim Riaz <asim.riaz@gmail.com>
2025-05-19 10:47:57 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-import express  from "express";
+import express from "express";
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import indexRouter from './routes/index.js'
 import cors from 'cors';
-    
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const PORT = process.env.PORT || 4500
 
 express()
-    .use(express.static(path.join(__dirname, 'dist')))
     .use(express.json())
     .use(cors())
-    .get("/", (req, res)=> res.sendFile(path.join(__dirname, 'index.html')))
+    .use(express.static(path.join(__dirname, 'dist')))
+    .get('*', (req, res) => res.sendFile(path.join(__dirname, 'dist', 'index.html')))
     .use('/api', indexRouter)
     .listen(PORT, () => console.log(`Server is listening on http://localhost:${PORT}`));
